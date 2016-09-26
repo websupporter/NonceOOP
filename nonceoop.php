b84e180388f2d458bf9c70c8bfcfcd9b5a99dc2f
websupporter <webmaster@websupporter.net>
2016-09-26 15:50:53 +0300

    gpl

--- nonceoop.php
+++ nonceoop.php
@@ -2,6 +2,24 @@
 	/**
 	 * NonceOOP
 	 * This library helps you to use WordPress Nonces in an objectoriented environment.
+	 *
+	 * @version 1.0.0
+	 * @author websupporter
+	 *
+	 * @license: GPL2 
+	 *
+	 * NonceOOP is free software: you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation, either version 2 of the License, or
+	 * any later version.
+	 *
+	 * NonceOOP is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	 * GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public License
+	 * along with NonceOOP. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.html.
 	 **/
 
 	class NonceOOP {
